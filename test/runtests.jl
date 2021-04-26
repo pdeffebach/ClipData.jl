3ba47cc46d6d1585840d69a75b867ce892a4cff1
Peter Deffebach <p.deffebach@gmail.com>
2021-04-26 17:25:18 -0400

    finish tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -59,19 +59,98 @@ end
 end
 
 @testset "tablemwe" begin
+    """
+    a b
+    1 2
+    3 4
+    """ |> clipboard
 
+    s = tablemwe(; returnstring=true)
+    s_correct =
+"""
+df = \"\"\"
+a,b
+1,2
+3,4
+\"\"\" |> IOBuffer |> CSV.File"""
+
+    @test s == s_correct
+
+
+    t = (a = [1, 3], b = [2, 4])
+    s = tablemwe(t; returnstring = true)
+
+    @test s == s_correct
 end
 
 @testset "arraymwe" begin
+    """
+    1 2
+    3 4
+    """ |> clipboard
 
+    s = arraymwe(; returnstring=true)
+    s_correct =
+"""
+X = \"\"\"
+1,2
+3,4
+\"\"\" |> IOBuffer |> CSV.File |> Tables.matrix"""
+
+    @test s == s_correct
+
+
+    t = [1 2; 3 4]
+    s = arraymwe(t; returnstring = true)
+
+    @test s == s_correct
+
+    """
+    1
+    2
+    3
+    4
+    """ |> clipboard
+
+    s = arraymwe(; returnstring=true)
+
+    s_correct =
+"""
+x = \"\"\"
+1
+2
+3
+4
+\"\"\" |> IOBuffer |> CSV.File |> Tables.matrix |> vec"""
+
+    @test s == s_correct
+
+    x = [1, 2, 3, 4]
+
+    s = arraymwe(x; returnstring=true)
+    @test s == s_correct
 end
 
 @testset "@tablemwe" begin
+    # Can't think of a way to test. Just check
+    # for errors.
 
+    mytable = (a = [1, 2], b = [3, 4])
+
+    @tablemwe mytable
 end
 
 @testset "@arraymwe" begin
+    # Can't think of a way to test. Just check
+    # for errors.
 
+    myarray = [1 2; 3 4]
+
+    @arraymwe myarray
+
+    myvector = [1, 2, 3, 4]
+
+    @arraymwe myvector
 end
 
 
